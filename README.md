ec3d617b7ef6ca1a21018164e8d19a9e2e75a10d
Siddharthpatil22 <smita7505@gmail.com>
2021-04-21 12:55:58 -0700

    added platform and readme file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# angryBirdsStage2.5
-Angry Birds stage 2.5 with Class Inheritance and Images
+git bash commands
+cd - change directory
+pwd - present working directory
+ls - list all files and folder under the currentdirectory
+git clone <git hub url> --downloads the code to local folder
+after git clone ,do the required changes
+git status - it will show the list of files modified
+git add <file name>
+git commit -m "type any message"
+github web url create empty repositry(without readme file)
+git remote add <repositry name> <github url for the new repositery>
+git push -u <github url for the new repositery>
--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup(){
     world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  ground = new Ground(600,height,1200,20);
+   
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    platform1=new Ground(150,305,300,170)
 }
 
 function draw(){
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    platform1.display(); 
     box3.display();
     box4.display();
     pig3.display();
@@ -60,5 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    
 }
